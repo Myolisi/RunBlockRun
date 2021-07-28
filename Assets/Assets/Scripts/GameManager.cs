8f1ab159be8abda8052041d4fadfe7e741f1c20a
myolisi <myolisi@ngubentombi.co.za>
2021-07-28 20:29:40 +0200

    Add project files

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static int currentScore;
+    public static int highScore;
+    public static int currentLevel = 0;
+    public static int unlockedLevel;
+
+
+    public static void CompleteLevel()
+    {
+
+        //Application.LoadLevel(currentLevel);
+        currentLevel += 1;
+        SceneManager.LoadScene(currentLevel); //increment by 1 after loading a level 
+                                              //currentLevel += 1;
+
+        Debug.Log(currentLevel);
+
+        if (currentLevel >= 2)
+        {
+            Debug.Log("Levels complete");
+            currentLevel = 0;
+        }
+
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(currentLevel);
+        SceneManager.UnloadScene(2);
+        Debug.Log(currentLevel);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
